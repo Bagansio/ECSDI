e77f1c7084e22e8cc246792d8eb651fad104996f
Cristian Mesa Sanchez <cristian.mesa.sanchez@estudiantat.upc.edu>
2022-05-17 16:01:17 +0200

    mostradorAgent

--- agents/MostradorAgent.py
+++ agents/MostradorAgent.py
@@ -336,9 +336,77 @@ def register_message():
 
 
 
+@app.route("/stop")
+def stop():
+    """
+    Entrypoint que para el agente
 
+    :return:
+    """
+    tidyup()
+    shutdown_server()
+    return "Parando Servidor"
 
+@app.route("/comm")
+def comunicacion():
+    """
+    Entrypoint de comunicacion del agente
+    Simplemente retorna un objeto fijo que representa una
+    respuesta a una busqueda de hotel
 
+    Asumimos que se reciben siempre acciones que se refieren a lo que puede hacer
+    el agente (buscar con ciertas restricciones, reservar)
+    Las acciones se mandan siempre con un Request
+    Prodriamos resolver las busquedas usando una performativa de Query-ref
+    """
+    global dsgraph
+    global mss_cnt
+
+    logger.info('Peticion de informacion recibida')
+
+    # Extraemos el mensaje y creamos un grafo con el
+    message = request.args['content']
+    gm = Graph()
+    gm.parse(data=message, format='xml')
+
+    msgdic = get_message_properties(gm)
+
+    # Comprobamos que sea un mensaje FIPA ACL
+    if msgdic is None:
+        # Si no es, respondemos que no hemos entendido el mensaje
+        gr = build_message(
+            Graph(), ACL['not-understood'], sender=MostradorAgent.uri, msgcnt=mss_cnt)
+    else:
+        # Obtenemos la performativa
+        perf = msgdic['performative']
+
+        if perf != ACL.request:
+            # Si no es un request, respondemos que no hemos entendido el mensaje
+            gr = build_message(
+                Graph(), ACL['not-understood'], sender=MostradorAgent.uri, msgcnt=mss_cnt)
+        else:
+            # Extraemos el objeto del contenido que ha de ser una accion de la ontologia de acciones del agente
+            # de registro
+
+            # Averiguamos el tipo de la accion
+            if 'content' in msgdic:
+                content = msgdic['content']
+                accion = gm.value(subject=content, predicate=RDF.type)
+
+                if accion == ECSDI.BuscarProductos:
+                    gr = buscarProductos(content, gm)
+            # Aqui realizariamos lo que pide la accion
+            # Por ahora simplemente retornamos un Inform-done
+            gr = build_message(Graph(),
+                               ACL['inform'],
+                               sender=MostradorAgent.uri,
+                               msgcnt=mss_cnt,
+                               receiver=msgdic['sender'], )
+    mss_cnt += 1
+
+    logger.info('Respondemos a la peticion')
+
+    return gr.serialize(format='xml')
 
 def tidyup():
     """
